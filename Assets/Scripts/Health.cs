d48cfb8b0886cdf2d14f2f28c45a51fba0077ef5
tannerhunt5 <tannerhunt5@tamu.edu>
2018-06-11 19:58:23 -0600

    Initial commit

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour
+{
+
+    public const int MaxHealth = 100;
+    //public int CurrentHealth = MaxHealth;
+
+    public RectTransform HealthBar;
+
+    [SyncVar(hook = "OnChangeHealth")]
+    public int CurrentHealth = MaxHealth;
+
+    public void TakeDamage(int Amount)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        CurrentHealth -= Amount;
+        Debug.Log(CurrentHealth);
+        if (CurrentHealth <= 0)
+        {
+            CurrentHealth = 0;
+            Debug.Log("Dead!");
+            
+        }
+
+       
+    }
+
+    void OnChangeHealth(int CurrentHealth)
+    {
+        HealthBar.sizeDelta = new Vector2(CurrentHealth, HealthBar.sizeDelta.y);
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public GameObject BulletPrefab;
+    public Transform BulletSpawn;
+
+    void Update()
+    {
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        var Bullet = (GameObject)Instantiate(BulletPrefab, BulletSpawn.position, BulletSpawn.rotation);
+
+        Bullet.GetComponent<Rigidbody>().velocity = Bullet.transform.forward * 60;
+
+        NetworkServer.Spawn(Bullet);
+
+        Destroy(Bullet, 2.0f);
+    }
+}
